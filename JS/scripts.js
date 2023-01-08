7ab06b9a6d84b665b86a2cc0bc75311fa41024d6
JrSantiago <jrsantiaggo@gmail.com>
2023-01-08 14:21:11 -0300

    interação com menu

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,26 @@
-function Person(name, age, acao){
-    this.name = name,
-    this.age = age,
-    this.acao = acao
+// interação com um menu
+let items = []
+let escolha
+
+while(escolha != 3){
+    escolha = Number(prompt(`Olá usuário! Digite o número da opção desejada
+        1. Cadastrar um item na lista
+        2. Mostrar itens cadastrados
+        3. Sair do programa`))
+
+    if(escolha == 1){
+        let item = prompt("Digite o item")
+        items.push(item)
+    }else if(escolha == 2){
+        if(items.length != 0){
+            alert(items)
+        }else{
+            alert('nenhum item cadastrado')
+        }
+    }else if(escolha == 3){
+        break
+    }else{
+        alert("não existe essa opção")
+    }
+
 }
-
-let junior = new Person("junior", 20, (anos)=>{
-    alert(`programando desde os ${anos} anos de idade`)
-})
-
-junior.acao(16)
