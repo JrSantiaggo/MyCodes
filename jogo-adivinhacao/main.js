2f3f9dae36a79dc2e947d67acdc259950083b5d3
JrSantiago <jrsantiaggo@gmail.com>
2023-01-10 06:56:17 -0300

    interface biscoito da sorte

--- jogo-adivinhacao/main.js
+++ jogo-adivinhacao/main.js
@@ -1,33 +1,33 @@
-const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-let randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
+// const screen1 = document.querySelector(".screen1")
+// const screen2 = document.querySelector(".screen2")
+// let randomNumber = Math.round(Math.random() * 10)
+// let xAttempts = 1
 
-function handleTryClick(event){
-    event.preventDefault()
+// function handleTryClick(event){
+//     event.preventDefault()
 
-    const inputNumber = document.querySelector("#inputNumber")
+//     const inputNumber = document.querySelector("#inputNumber")
 
-    if(Number(inputNumber.value) == randomNumber){
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+//     if(Number(inputNumber.value) == randomNumber){
+//         screen1.classList.add("hide")
+//         screen2.classList.remove("hide")
 
-        xAttempts == 1?document.querySelector(".screen2 h2")
-        .innerText = `Acertou em ${xAttempts} tentativa`:document.querySelector(".screen2 h2")
-        .innerText = `Acertou em ${xAttempts} tentativas`
+//         xAttempts == 1?document.querySelector(".screen2 h2")
+//         .innerText = `Acertou em ${xAttempts} tentativa`:document.querySelector(".screen2 h2")
+//         .innerText = `Acertou em ${xAttempts} tentativas`
        
-    }
-    inputNumber.value = ""
-    xAttempts++
-}
+//     }
+//     inputNumber.value = ""
+//     xAttempts++
+// }
 
-const btnTry = document.querySelector("#btnTry")
-btnTry.addEventListener('click', handleTryClick) //função callback
+// const btnTry = document.querySelector("#btnTry")
+// btnTry.addEventListener('click', handleTryClick) //função callback
 
-const btnAgain = document.querySelector("#btnAgain")
-btnAgain.addEventListener('click', function(){
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-    randomNumber = Math.round(Math.random() * 10)
-    xAttempts = 1
-})
+// const btnAgain = document.querySelector("#btnAgain")
+// btnAgain.addEventListener('click', function(){
+//     screen1.classList.remove("hide")
+//     screen2.classList.add("hide")
+//     randomNumber = Math.round(Math.random() * 10)
+//     xAttempts = 1
+// })
