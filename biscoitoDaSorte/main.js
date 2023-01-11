8783ec6e46f0c30dcd678c3564cd2f00ab3dc50d
JrSantiago <jrsantiaggo@gmail.com>
2023-01-10 22:27:43 -0300

    finalização da lógica

--- biscoitoDaSorte/main.js
+++ biscoitoDaSorte/main.js
@@ -1,33 +1,36 @@
+//variáveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const biscuit = document.querySelector("#abrirBiscoito")
+const btnAgain = document.querySelector("#btnAgain")
+const luckyPhraseRandom = document.querySelector(".screen2 p")
 let randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
+let luckyPhrase = ["A vida trará coisas boas se tiver paciência.",
+"Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.",
+"Não compense na ira o que lhe falta na razão.",
+"Defeitos e virtudes são apenas dois lados da mesma moeda.",
+"A maior de todas as torres começa no solo.",
+"Não há que ser forte. Há que ser flexível.",
+"Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?",
+"Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.",
+"A juventude não é uma época da vida, é um estado de espírito.",
+"Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.",]
 
-function handleTryClick(event){
-    event.preventDefault()
-
-    const inputNumber = document.querySelector("#inputNumber")
-
-    if(Number(inputNumber.value) == randomNumber){
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
-
-        xAttempts == 1?document.querySelector(".screen2 h2")
-        .innerText = `Acertou em ${xAttempts} tentativa`:document.querySelector(".screen2 h2")
-        .innerText = `Acertou em ${xAttempts} tentativas`
-       
-    }
-    inputNumber.value = ""
-    xAttempts++
+//funções
+function openBiscuit(){
+    screen1.classList.add("hide")
+    screen2.classList.remove("hide")
+    randomSentence()
+}
+function openBiscuitAgain(){
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+    randomNumber = Math.round(Math.random() * 10)
+}
+function randomSentence(){
+    luckyPhraseRandom.innerText = luckyPhrase[randomNumber]
 }
 
-const btnTry = document.querySelector("#btnTry")
-btnTry.addEventListener('click', handleTryClick) //função callback
-
-const btnAgain = document.querySelector("#btnAgain")
-btnAgain.addEventListener('click', function(){
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-    randomNumber = Math.round(Math.random() * 10)
-    xAttempts = 1
-})
+//eventos
+biscuit.addEventListener('click', openBiscuit)
+btnAgain.addEventListener('click', openBiscuitAgain)
