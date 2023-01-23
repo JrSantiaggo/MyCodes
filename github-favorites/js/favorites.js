21597f212e3684f5b1ec8c2776f8ebc1dfb2cf5a
JrSantiago <jrsantiaggo@gmail.com>
2023-01-23 20:18:52 -0300

    concluindo projeto git favorites

--- github-favorites/js/favorites.js
+++ github-favorites/js/favorites.js
@@ -1,3 +1,22 @@
+export class GithubUser {
+  static search(username){
+    const endpoint = `https://api.github.com/users/${username}`
+    return fetch(endpoint).then(data => data.json())
+    .then((data) => {
+
+      const {login, name, public_repos, followers} = data
+
+      return {
+        login,
+        name,
+        public_repos,
+        followers,
+      }
+    })
+  }
+}
+
+
 export class Favorites {
   constructor(root){
     this.root = document.querySelector(root)
@@ -5,22 +24,18 @@ export class Favorites {
   }
 
   load(){
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+  async add(username){
+    const user =  await GithubUser.search(username)
+  }
+  
+  delete(user) {
+    const filteredEntries = this.entries
+      .filter(entry => entry.login !== user.login)
 
-    this.entries = [
-      {
-      login: 'JrSantiaggo',
-      name: 'Junior Santiago',
-      public_repos: '40',
-      followers: '100',
-      },
-      {
-      login: 'Diego3g',
-      name: 'Diego Fernandes',
-      public_repos: '70',
-      followers: '90000',
-      },
-    ]
-    
+    this.entries = filteredEntries
+    this.update()
   }
 }
 
@@ -30,6 +45,15 @@ export class FavoritesView extends Favorites{
 
     this.tbody = this.root.querySelector('table tbody')
     this.update()
+    this.onadd()
+  }
+
+  onadd(){
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const {value} = this.root.querySelector('.search input')
+      this.add(value)
+    }
   }
 
   update(){
@@ -43,6 +67,12 @@ export class FavoritesView extends Favorites{
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if(isOk){
+          this.delete(user)
+        }
+      }
 
       this.tbody.append(row)
     })
