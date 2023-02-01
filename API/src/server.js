67ad968ee305bf8d4a9475d9622d535bd578da25
JrSantiago <jrsantiaggo@gmail.com>
2023-02-01 19:59:28 -0300

    praticando método post/body

--- API/src/server.js
+++ API/src/server.js
@@ -2,20 +2,13 @@ const { request } = require("express");
 const express = require("express");
 
 const app = express();
-
-app.get("/hello/:id/:user", (request, response) => {
-  const {id, user} = request.params
-  response.send(`
-    id da mensagem: ${id} para o usuário ${user}
-  `)
-})
-
+app.use(express.json());
 
 //product?page=2&limit=10 = optional dps do ?
-app.get("/product", (request, response) => {
-  const {page, limit} = request.query
-
-  response.send(`Produto na página ${page} com limite de ${limit} cards`)
+app.post("/users", (request, response) => {
+  const { name, email, password} = request.body
+ 
+  response.json({ name, email, password})
 })
 
 const PORT = 3333;
