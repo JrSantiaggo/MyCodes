cd46836340073fd6370aeece12e05d0208687c54
JrSantiago <jrsantiaggo@gmail.com>
2023-02-14 10:06:09 -0300

    função de cadastrar notas, links e tags

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -1,9 +1,11 @@
 const {Router} = require("express")
 
 const usersrRouter = require("./users.routes")
+const notesRouter = require("./notes.routes")
 
 const routes = Router()
 
 routes.use("/users", usersrRouter)
+routes.use("/notes", notesRouter)
 
 module.exports = routes
