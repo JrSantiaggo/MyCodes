bc00f724504d03f1adcde5598678592b1f570d86
JrSantiago <jrsantiaggo@gmail.com>
2023-02-17 02:22:22 -0300

    finalizando api

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -2,10 +2,12 @@ const {Router} = require("express")
 
 const usersrRouter = require("./users.routes")
 const notesRouter = require("./notes.routes")
+const tagsRouter = require("./tags.routes")
 
 const routes = Router()
 
 routes.use("/users", usersrRouter)
 routes.use("/notes", notesRouter)
+routes.use("/tags", tagsRouter)
 
 module.exports = routes
